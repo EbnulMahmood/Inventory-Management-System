279568dc2ac4a955887761e1a225f214eab99dfc
shovan <shovonmahmood67@gmail.com>
2022-10-08 18:02:41 +0600

    add n-tier

---
+++ InventoryManagementSystem.Application/Services/CategoryService.cs
@@ -0,0 +1,19 @@
+﻿using InventoryManagementSystem.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Application.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _repository;
+
+        public CategoryService(ICategoryRepository repository)
+        {
+            _repository = repository;
+        }
+    }
+}
